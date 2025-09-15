0cdcd76e52e99ad5ff1b443940fcaa2f8146ad71
UltralyticsAssistant <web@ultralytics.com>
2025-09-15 16:43:39 +0000

    Auto-format by https://ultralytics.com/actions

--- yolo_sahi_infer.py
+++ yolo_sahi_infer.py
@@ -1,21 +1,20 @@
 import os
+
 from sahi.models.ultralytics import UltralyticsDetectionModel
 from sahi.predict import get_sliced_prediction
-
 
 # 1 配置 YOLOv8 模型
 
 model = UltralyticsDetectionModel(
     model_path="E:/VisualProject/ultralytics-main/runs/detect/train2/weights/best.pt",  # 替换成你的训练权重
     confidence_threshold=0.3,
-    device="cpu"  # 如果有 GPU 改成 "cuda:0"
+    device="cpu",  # 如果有 GPU 改成 "cuda:0"
 )
 
 
 # 2 配置测试图片路径和输出路径
 image_folder = "E:/VisualProject/ultralytics-main/data/mycoco/test_images"  # 测试图片文件夹
-output_folder = "E:/VisualProject/ultralytics-main/sahi_results"           # 输出文件夹
-
+output_folder = "E:/VisualProject/ultralytics-main/sahi_results"  # 输出文件夹
 
 
 # 3 遍历文件夹里的所有 jpg 图片
@@ -31,13 +30,9 @@ for img_name in os.listdir(image_folder):
             slice_height=256,
             slice_width=256,
             overlap_height_ratio=0.2,
-            overlap_width_ratio=0.2
+            overlap_width_ratio=0.2,
         )
-
 
         # 4 保存可视化结果
-        result.export_visuals(
-            export_dir=output_folder,
-            file_name=f"{os.path.splitext(img_name)[0]}_pred.jpg"
-        )
+        result.export_visuals(export_dir=output_folder, file_name=f"{os.path.splitext(img_name)[0]}_pred.jpg")
 print("所有图片检测完成！")
